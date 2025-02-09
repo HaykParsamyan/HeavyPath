c68186d5cee6276cb27999c08dc104e9f56d5316
HaykParsamyan <haykparsamyan.onlyt@gmail.com>
2025-02-09 11:44:22 +0400

    Bug Fixes

--- app/src/main/java/com/example/heavypath_project/ProfileActivity.java
+++ app/src/main/java/com/example/heavypath_project/ProfileActivity.java
@@ -1,30 +1,110 @@
 package com.example.heavypath_project;
 
+import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.widget.ImageView;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class ProfileActivity extends AppCompatActivity {
 
-    private Button signOutButton;
+    private ImageView profileImage;
+    private TextView usernameTextView;
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
+    private static final int PICK_IMAGE = 2;
+    private String currentPhotoPath = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
-        signOutButton = findViewById(R.id.sign_out_button);
+        profileImage = findViewById(R.id.profile_image);
+        usernameTextView = findViewById(R.id.username);
 
-        signOutButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Navigate back to LoginActivity
-                Intent intent = new Intent(ProfileActivity.this, LoginActivity.class);
-                startActivity(intent);
-                finish(); // Optional: finish the ProfileActivity so the user cannot go back to it
+        // Set username
+        String username = getIntent().getStringExtra("USERNAME");
+        usernameTextView.setText(username);
+
+        profileImage.setOnClickListener(v -> showImagePickerDialog());
+    }
+
+    private void showImagePickerDialog() {
+        String[] options = {"Take Photo", "Choose from Gallery", "Cancel"};
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Choose your profile picture");
+        builder.setItems(options, (dialog, which) -> {
+            switch (options[which]) {
+                case "Take Photo":
+                    takePhotoFromCamera();
+                    break;
+                case "Choose from Gallery":
+                    choosePhotoFromGallery();
+                    break;
+                case "Cancel":
+                    dialog.dismiss();
+                    break;
             }
         });
+        builder.show();
+    }
+
+    private void takePhotoFromCamera() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+            }
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this, "com.example.heavypath_project.fileprovider", photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
+        }
+    }
+
+    private File createImageFile() throws IOException {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.US).format(new Date());
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                "JPEG_" + timeStamp + "_",
+                ".jpg",
+                storageDir
+        );
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    private void choosePhotoFromGallery() {
+        Intent pickPhoto = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(pickPhoto, PICK_IMAGE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK) {
+            File file = new File(currentPhotoPath);
+            Uri uri = Uri.fromFile(file);
+            profileImage.setImageURI(uri);
+        } else if (requestCode == PICK_IMAGE && resultCode == Activity.RESULT_OK) {
+            Uri selectedImage = data.getData();
+            profileImage.setImageURI(selectedImage);
+        }
     }
 }
